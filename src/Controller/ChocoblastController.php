f5117fcc6291253415c72642459224b387cb9358
mithridatem <mathieumithridate@adrar-formation.com>
2023-11-28 09:46:05 +0100

    refacto chocoblast

--- src/Controller/ChocoblastController.php
+++ src/Controller/ChocoblastController.php
@@ -13,40 +13,54 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Serializer\SerializerInterface;
-
+use App\Service\ApiTokenService;
+use App\Service\UtilsService;
 class ChocoblastController extends AbstractController
 {
+    private $serializer;
+    private $chocoblastRepository;
+    private $userRepository;
+    private $em;
+    public function __construct(ChocoblastRepository $chocoblastRepository,
+    UserRepository $userRepository,SerializerInterface $serializerInterface,
+    EntityManagerInterface $entityManagerInterface){
+        $this->serializer = $serializerInterface;
+        $this->chocoblastRepository = $chocoblastRepository;
+        $this->userRepository = $userRepository;
+        $this->em = $entityManagerInterface;
+    }
     //méthode pour ajouter un chocoblast
     #[Route('/chocoblast/add', name:'app_chocoblast_add')]
-    public function chocoblastAdd(Request $request,EntityManagerInterface $em,
-    ChocoblastRepository $chocoblastRepository,UserRepository $userRepository,
-    SerializerInterface $serializerInterface):Response{
+    public function chocoblastAdd(Request $request):Response{
+        $message = "";
+        $code = 200;
         $json = $request->getContent();
         //test si le json est valide
         if($json){
-            $data = $serializerInterface->decode($json, 'json');
+            $data = $this->serializer->decode($json, 'json');
             $choco = new Chocoblast();
-            $choco->setTitle($data['title']);
-            $choco->setContent($data['content']);
-            $choco->setCreationDate(new \DateTimeImmutable($data['creation_date']));
-            $choco->setAuthor($userRepository->find($data['author']['id']));
-            $choco->setTarget($userRepository->find($data['target']['id']));
+            $choco->setTitle(UtilsService::cleanInput($data["title"]));
+            $choco->setContent(UtilsService::cleanInput($data["content"]));
+            $choco->setCreationDate(new \DateTimeImmutable(UtilsService::cleanInput($data["creation_date"])));
+            $choco->setAuthor($this->userRepository->find(UtilsService::cleanInput($data["author"]["id"])));
+            $choco->setTarget($this->userRepository->find(UtilsService::cleanInput($data["target"]["id"])));
             $choco->setActivated(true);
-            $em->persist($choco);
-            $em->flush();
-            return $this->json(['error'=>"le chocoblast a été ajouté en bdd"],200,['Content-Type'=>'application/json',
-            'Access-Control-Allow-Origin'=>'*']);
+            $this->em->persist($choco);
+            $this->em->flush();
+            $message = ['error'=>"le chocoblast : ".$choco->getTitle()." a été ajouté en bdd"];
         }
         //test si le json est invalide
         else{
-            return $this->json(['Error'=>'Json invalide'],400,['Content-Type'=>'application/json',
-            'Access-Control-Allow-Origin'=>'*']);
+            $message = ['Error'=>'Json invalide'];
+            $code = 400;
         }
+        return $this->json($message,$code,['Content-Type'=>'application/json',
+        'Access-Control-Allow-Origin'=>'*']);
     }
     //méthode pour afficher tous les chocoblasts
     #[Route('/chocoblast/all', name:'app_chocoblast_all')]
-    public function getAllChocoblast(ChocoblastRepository $chocoblastRepository):Response{
-        $chocos = $chocoblastRepository->findAll();
+    public function getAllChocoblast():Response{
+        $chocos = $this->chocoblastRepository->findAll();
         //test si le chocoblast existe
         if($chocos){
             return $this->json($chocos,200,['Content-Type'=>'application/json',
@@ -60,8 +74,8 @@ class ChocoblastController extends AbstractController
     }
     //méthode pour afficher un chocoblast par son id
     #[Route('/chocoblast/id/{id}', name:'app_chocoblast_id')]
-    public function getChocoblastById($id,ChocoblastRepository $chocoblastRepository):Response{
-        $choco = $chocoblastRepository->find($id);
+    public function getChocoblastById($id):Response{
+        $choco = $this->chocoblastRepository->find($id);
         if($choco){
             return $this->json($choco,200,['Content-Type'=>'application/json',
             'Access-Control-Allow-Origin'=>'*'],['groups'=>'choco']);
